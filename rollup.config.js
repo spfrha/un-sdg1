34428af8b97ae462e6dd12f5aac40942c6b1f198
spfrha <spencerharr1ty@gmail.com>
2024-10-17 12:09:29 -0400

    images fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'index.html',
@@ -21,7 +22,11 @@ export default {
       minify: true,
     }),
     /** Resolve bare module imports */
-    nodeResolve(),
+    nodeResolve(), copy({
+      targets: [
+        { src: 'lib/', dest: 'dist' }
+      ]
+    }),
     /** Minify JS, compile JS to a lower language target */
     esbuild({
       minify: true,
